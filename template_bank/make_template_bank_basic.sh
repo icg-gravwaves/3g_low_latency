93ba4d6ad0963f4b1d07fea91d82b0aff5b9cc73
GarethDaviesGW <gareth.cabourndavies@ligo.org>
2023-11-08 09:46:15 +0000

    template bank making script, approximate

---
+++ template_bank/make_template_bank_basic.sh
@@ -0,0 +1,15 @@
+pycbc_geom_nonspinbank \
+  --verbose \
+  --random-seed 1865 \
+  --min-match 0.975 \
+  --output-file CE_bank_v1.hdf \
+  --pn-order threePointFivePN \
+  --f-low 7 \
+  --f-upper 1024 \
+  --output-f-final \
+  --delta-f 0.01 \
+  --min-mass1 1 \
+  --max-mass1 2.5 \
+  --min-mass2 1 \
+  --max-mass2 2.5 \
+  --psd-model CosmicExplorerP1600143
